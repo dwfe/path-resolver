3facb78160411fe8b7edce20011ba0d5b0f90c2c
z <alex@dwfe.ru>
2022-04-20 22:56:22 +0300

    .

--- src/__tests__/route/constructor-validation.test.ts
+++ src/__tests__/route/constructor-validation.test.ts
@@ -7,8 +7,14 @@ describe('Route.constructor, incorrect use', () => {
 
   test('path', () => {
     toThrow(() => rootPath('/'), `Invalid route's "path" [cannot start with a slash]`);
+    toThrow(() => rootPath('/user'), `Invalid route's "path" [cannot start with a slash]`);
+    toThrow(() => rootPath('/:user'), `Invalid route's "path" [cannot start with a slash]`);
     toThrow(() => path('', '/'), `Invalid route's "path" [cannot start with a slash]`);
+    toThrow(() => path('', '/user'), `Invalid route's "path" [cannot start with a slash]`);
+    toThrow(() => path('', '/:user'), `Invalid route's "path" [cannot start with a slash]`);
     toThrow(() => path('', ''), `Invalid route's "path" [non-root empty]`);
+    toThrow(() => path('user', ''), `Invalid route's "path" [non-root empty]`);
+    toThrow(() => path(':user', ''), `Invalid route's "path" [non-root empty]`);
     toThrow(() => path('', '(.*)'), 'Incorrect location for "(.*)" path. Position it at the root');
   });
 
