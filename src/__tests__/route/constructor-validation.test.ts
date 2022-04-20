51b5d3b932f1772b218a2d441457f94df241cd5c
z <alex@dwfe.ru>
2022-04-21 00:56:50 +0300

    .

--- src/__tests__/route/constructor-validation.test.ts
+++ src/__tests__/route/constructor-validation.test.ts
@@ -1,6 +1,7 @@
 import {describe} from '@jest/globals';
 import {path, rootPath} from './constructor-normalization.test'
 import {toThrow} from '../util';
+import {Route} from '../../core/a/route'
 
 
 describe('Route.constructor, incorrect use', () => {
@@ -13,6 +14,16 @@ describe('Route.constructor, incorrect use', () => {
     toThrow(() => path('', '/user'), `Invalid route's "path" [cannot start with a slash]`);
     toThrow(() => path('', '/:user'), `Invalid route's "path" [cannot start with a slash]`);
     toThrow(() => path('', ''), `Invalid route's "path" [non-root empty]`);
+    toThrow(() => Route.of({
+      path: '', children: [
+        {path: ''}
+      ]
+    }), `Invalid route's "path" [non-root empty]`);
+    toThrow(() => Route.of({
+      path: 'user', children: [
+        {path: ''}
+      ]
+    }), `Invalid route's "path" [non-root empty]`);
     toThrow(() => path('user', ''), `Invalid route's "path" [non-root empty]`);
     toThrow(() => path(':user', ''), `Invalid route's "path" [non-root empty]`);
     toThrow(() => path('', '(.*)'), 'Incorrect location for "(.*)" path. Position it at the root');
