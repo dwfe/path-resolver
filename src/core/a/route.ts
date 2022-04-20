2eb210670b841100e8285a898929be538cad5c97
z <alex@dwfe.ru>
2022-04-20 10:41:51 +0300

    .

--- src/core/a/route.ts
+++ src/core/a/route.ts
@@ -5,13 +5,19 @@ export class Route extends IRoute {
   constructor(public readonly origin: IRoute,
               public readonly parent?: Route) {
     super();
-    const {path, component, redirectTo, customTo, action, children, canActivate, canDeactivate, note, name} = origin;
-
-    if (path[0] === '/') {
-      console.error(`Invalid configuration of route, because path "${path}" cannot start with a slash`);
-      throw new Error(`Invalid route's "path"`);
-    }
+    this.path = Route.normalizePath(origin);
 
   }
 
+
+  static normalizePath(route: IRoute, parent?: IRoute): string {
+    if (route.path[0] === '/') {
+      console.error(`Invalid configuration of route, because path "${route.path}" cannot start with a slash`);
+      throw new Error(`Invalid route's "path"`);
+    }
+    if (route.path === '')
+      return '/';
+    let prefix = parent && parent.path !== '/' ? parent.path : '';
+    return prefix + '/' + route.path;
+  }
 }
